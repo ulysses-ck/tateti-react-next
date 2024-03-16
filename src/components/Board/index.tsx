a278f8d0075d66bfbba6800a59a42ba4582c7be9
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-16 12:22:16 -0300

    feat: add logic to board.
    - Add board state to handle state of the game.
    - Add player state to handle turns of players.
    - Add function to handle every cell modification itself.

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,16 +1,52 @@
 "use client";
+import { useState } from "react";
+
 export default function Board() {
+	// board is a 3x3 grid of cells
+	const [board, setBoard] = useState([
+		["", "", ""],
+		["", "", ""],
+		["", "", ""],
+	]);
+
+	// player is either "X" or "O"
+	const [player, setPlayer] = useState("X");
+
+	const handleClickCell = (rowIndex: number, cellIndex: number) => {
+		const newBoard = board.map((row, r) => {
+			if (rowIndex === r) {
+				return row.map((cell, c) => {
+					if (cellIndex === c) {
+						if (cell === "") {
+							setPlayer((prevPlayer) =>
+								prevPlayer === "X" ? "O" : "X",
+							);
+							return player;
+						}
+					}
+					return cell;
+				});
+			}
+			return row;
+		});
+		setBoard(newBoard);
+	};
+
 	return (
-		<div className="grid grid-cols-3 grid-row-3 gap-2 bg-black w-fit">
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
-			<div className="w-[80px] h-[80px] bg-red-300"></div>
+		<div className="grid grid-cols-3 grid-row-3 gap-2 bg-black w-fit h-fit">
+			{board.map((row, rowIndex) => {
+				return row.map((cell, cellIndex) => {
+					return (
+						<div
+							className="w-[80px] h-[80px] bg-red-300"
+							key={`${rowIndex}${cellIndex}`}
+							onClick={() => handleClickCell(rowIndex, cellIndex)}
+						>
+							{cell}
+						</div>
+					);
+				});
+			})}
 		</div>
 	);
 }
