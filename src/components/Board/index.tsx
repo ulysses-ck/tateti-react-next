796ee3a645e5d40945bb64c5595ea19e691e592e
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-16 12:06:05 -0300

    feat: Board component.

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+export default function Board() {
+	return (
+		<div className="grid grid-cols-3 grid-row-3 gap-2 bg-black w-fit">
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+			<div className="w-[80px] h-[80px] bg-red-300"></div>
+		</div>
+	);
+}
