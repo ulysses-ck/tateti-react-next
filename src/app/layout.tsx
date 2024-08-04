7b42a6a7b0acef159550451af93d9974b47c5e1a
ulysses-ck <ulisesapaza.280@gmail.com>
2024-08-04 02:19:21 -0300

    style: adapt ui to fit entire screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>
+			<body className="min-h-screen min-w-screen flex flex-col justify-between">
 				<Navbar />
 				{children}
 				<Footer />
