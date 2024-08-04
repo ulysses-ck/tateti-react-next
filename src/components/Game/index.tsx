1a66a689b9f55bd4827d8eaf27dfa8e371bc37c8
ulysses-ck <ulisesapaza.280@gmail.com>
2024-08-04 02:22:59 -0300

    fix: logic and puts winner

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 // react
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // components
 import Board from "../Board";
@@ -19,41 +19,45 @@ export default function Game() {
 	// player is either "X" or "O"
 	const [player, setPlayer] = useState("X");
 
+	// game's winner
+	const [winner, setWinner] = useState("");
+
+	const handleClickPlay = () => {
+		setIsPlaying(true);
+	};
+
+	const handleClickReset = () => {
+		setBoard([
+			["", "", ""],
+			["", "", ""],
+			["", "", ""],
+		]);
+		setIsPlaying(false);
+	};
+
 	useEffect(() => {
-		// check if there is a winner
 		const checkWinner = (board: string[][], player: string) => {
 			// check rows
 			for (let i = 0; i < 3; i++) {
 				if (
-					board[i][0] === player &&
-					board[i][1] === player &&
-					board[i][2] === player
-				) {
-					return true;
-				}
-			}
-			// check columns
-			for (let i = 0; i < 3; i++) {
-				if (
-					board[0][i] === player &&
-					board[1][i] === player &&
-					board[2][i] === player
+					(board[i][0] === player &&
+						board[i][1] === player &&
+						board[i][2] === player) ||
+					(board[0][i] === player &&
+						board[1][i] === player &&
+						board[2][i] === player)
 				) {
 					return true;
 				}
 			}
 			// check diagonals
 			if (
-				board[0][0] === player &&
-				board[1][1] === player &&
-				board[2][2] === player
-			) {
-				return true;
-			}
-			if (
-				board[0][2] === player &&
-				board[1][1] === player &&
-				board[2][0] === player
+				(board[0][0] === player &&
+					board[1][1] === player &&
+					board[2][2] === player) ||
+				(board[0][2] === player &&
+					board[1][1] === player &&
+					board[2][0] === player)
 			) {
 				return true;
 			}
@@ -75,46 +79,30 @@ export default function Game() {
 		// check if the game is over
 		const checkGameOver = (board: string[][], player: string) => {
 			if (checkWinner(board, player)) {
-				alert(`${player} wins!`);
-				setBoard([
-					["", "", ""],
-					["", "", ""],
-					["", "", ""],
-				]);
+				setIsPlaying(false);
+				setWinner(player);
 			} else if (checkDraw(board)) {
-				alert("Draw!");
-				setBoard([
-					["", "", ""],
-					["", "", ""],
-					["", "", ""],
-				]);
+				setIsPlaying(false);
+				setWinner("Draw");
 			}
 		};
 
 		checkGameOver(board, player);
 	}, [board, player]);
 
-	const handleClickPlay = () => {
-		setIsPlaying(true);
-	};
-
-	const handleClickReset = () => {
-		setBoard([
-			["", "", ""],
-			["", "", ""],
-			["", "", ""],
-		]);
-		setIsPlaying(false);
-	};
 	return (
 		<section>
 			<h1 className="text-center text-2xl">
-				{!isPlaying ? "Board" : `Its ${player}'s turn!`}
+				{isPlaying
+					? `Its ${player}'s turn!`
+					: winner
+						? winner
+						: "Board"}
 			</h1>
 			<Board
 				board={board}
-				player={player}
 				setBoard={setBoard}
+				player={player}
 				setPlayer={setPlayer}
 				isPlaying={isPlaying}
 			/>
